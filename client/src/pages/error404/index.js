cfd6693def7b51a67016a3ffe4412a77d4aa94f7
Victor Pragana <victormerigue@gmail.com>
2020-11-13 23:32:13 -0300

    primeira rota para visualizar todos os produtos

---
+++ client/src/pages/error404/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Error404() {
+  return (
+    <>
+      <h2>Não encontramos nada... tente novamente...</h2>
+    </>
+  );
+}
--- client/src/pages/produtos/index.js
+++ client/src/pages/produtos/index.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import api from "../../services/api";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 
 export default function Produtos() {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    async function buscaProdutos() {
+      const res = await api.get("produto").catch((err) => console.error("err", err));
+      setProdutos(res.data);
+    }
+
+    buscaProdutos();
+  }, []);
+
   return (
-    <>
-      <h1>Todos os produtos</h1>
-    </>
+    <Container>
+      <Typography variant="h2" color="inherit">
+        Todos os produtos
+      </Typography>
+
+      {produtos &&
+        produtos.map((produto) => (
+          <Card key={produto._id}>
+            <CardContent>
+              <Typography>Nome do produto: {produto.nome}</Typography>
+              <Typography>Preço: {produto.preco}</Typography>
+              <Typography>Quantidade: {produto.quantidade}</Typography>
+              <Typography>Codigo de barras: {produto.codigo}</Typography>
+            </CardContent>
+          </Card>
+        ))}
+    </Container>
   );
 }
