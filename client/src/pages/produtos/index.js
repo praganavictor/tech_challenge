83dbdb5c7c3a01060eb9a8e0c2809557014256eb
Victor Pragana <victormerigue@gmail.com>
2020-11-16 02:34:31 -0300

    Função para filtrar por preço ou quantidade
    A função precisa de um bugfix pq no momento da seleção ele aparentemente guarda em memoria e só altera na segunda vez q é solicitado

--- client/src/pages/produtos/index.js
+++ client/src/pages/produtos/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 import api from "../../services/api";
+import "./index.css";
 
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
@@ -9,11 +10,16 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
 
 export default function Produtos() {
   const history = useHistory();
 
   const [produtos, setProdutos] = useState([]);
+  const [selecao, setSelecao] = useState(0);
 
   useEffect(() => {
     async function buscaProdutos() {
@@ -24,6 +30,24 @@ export default function Produtos() {
     buscaProdutos();
   }, []);
 
+  useEffect(() => {
+    let nProdutos;
+    switch (selecao) {
+      case "1":
+        nProdutos = produtos.sort((p1, p2) => p1.preco - p2.preco);
+        setProdutos(nProdutos);
+        break;
+
+      case "2":
+        nProdutos = produtos.sort((p1, p2) => p1.quantidade - p2.quantidade);
+        setProdutos(nProdutos);
+        break;
+
+      default:
+        break;
+    }
+  }, [produtos, selecao]);
+
   async function handleDelete(produto) {
     const response = await api.delete(`/produto/${produto}`);
     if (response.status === 200) {
@@ -32,14 +56,27 @@ export default function Produtos() {
   }
 
   return (
-    <Container maxWidth="md" align="center">
-      <Typography variant="h2" color="inherit">
+    <Container maxWidth="md">
+      <Typography variant="h2" color="inherit" align="center">
         Todos os produtos
       </Typography>
 
       <Button variant="contained" color="primary" href="produtos/criar">
         Adicionar Produto
       </Button>
+      <br></br>
+      <FormControl className="selection">
+        <InputLabel id="demo-simple-select-label">Selecione uma opção</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          value={selecao}
+          onChange={(e) => setSelecao(e.target.value)}
+        >
+          <MenuItem disabled value="0"></MenuItem>
+          <MenuItem value="1">Preço </MenuItem>
+          <MenuItem value="2">Quantidade </MenuItem>
+        </Select>
+      </FormControl>
 
       {produtos &&
         produtos.map((produto) => (
